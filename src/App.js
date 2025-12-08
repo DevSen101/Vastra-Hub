eb7bfa64f900d5989d9b7e20c6180b84dbf97939
Dev Sen <devksen.personal@gmail.com>
2025-12-08 18:31:32 +0530

    feat: add routing setup with new routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import './App.css';
-import HomePage from './pages/homepage/homepage.component';
+import React from "react";
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
+import { Routes, Route } from "react-router-dom";
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  );
+};
 
 function App() {
-  return <div>
-    <HomePage/>
-  </div>
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/hats" element={<HatsPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
