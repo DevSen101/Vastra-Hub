193976ee935f0ea3d676d885bce780a45061be54
Dev Sen <devksen.personal@gmail.com>
2025-12-08 23:23:40 +0530

    feat: add navigation using useNavigate in MenuItem component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,20 +1,25 @@
 import React from "react";
 import "./menu-item.styles.css";
+import { useNavigate } from "react-router-dom";
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
-    <div
-      className="background-image"
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-    />
+const MenuItem = ({ title, imageUrl, size, linkUrl, history, match }) => {
+  const navigate = useNavigate();
 
-    <div className="content">
-      <h1 className="title">{title.toUpperCase()}</h1>
-      <span className="subtitle">SHOP NOW</span>
+  return (
+    <div className={`${size} menu-item`} onClick={() => navigate(linkUrl)}>
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default MenuItem;
